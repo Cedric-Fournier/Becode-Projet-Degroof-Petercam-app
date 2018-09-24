65c155fb77b80e94939cebf761245f24ad62120b
CharlotteTusset <charlotte.tusset@gmail.com>
2018-09-24 16:56:43 +0200

    LoginPage-Front

--- DegroofPetercamApp/App.js
+++ DegroofPetercamApp/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './components/Login';
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Login />
       </View>
     );
   }
