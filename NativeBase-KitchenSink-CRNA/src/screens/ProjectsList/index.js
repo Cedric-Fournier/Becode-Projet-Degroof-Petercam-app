622e9a03443477eff92b107378ee751b479b2a9f
Massimo Regaglia <m.regaglia@gmail.com>
2018-09-25 16:46:20 +0200

    project_list_done

--- NativeBase-KitchenSink-CRNA/src/screens/ProjectsList/index.js
+++ NativeBase-KitchenSink-CRNA/src/screens/ProjectsList/index.js
@@ -16,6 +16,7 @@ import {
 import styles from "./styles";
 
 class ProjectsList extends Component {
+
   render() {
     return (
       <Container style={styles.container}>
@@ -35,11 +36,11 @@ class ProjectsList extends Component {
         </Header>
 
         <Content>
-          <NHCardShowcase/>
-          <NHCardShowcase/>
-          <NHCardShowcase/>
-          <NHCardShowcase/>
-          <NHCardShowcase/>
+          <NHCardShowcase projectTitle={"Project 1"} projectDate={"April 15, 2018"} projectText={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget neque euismod, tincidunt risus at, viverra lacus. In gravida nunc sed ex sollicitudin ultrices. Vivamus luctus, neque at facilisis euismod, turpis diam finibus libero, at malesuada risus eros a justo. Quisque dictum, quam vitae finibus blandit, erat urna imperdiet nisl."}/>
+          <NHCardShowcase projectTitle={"Project 2"} projectDate={"June 22, 2018"} projectText={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget neque euismod, tincidunt risus at, viverra lacus. In gravida nunc sed ex sollicitudin ultrices. Vivamus luctus, neque at facilisis euismod, turpis diam finibus libero, at malesuada risus eros a justo. Quisque dictum, quam vitae finibus blandit, erat urna imperdiet nisl."}/>
+          <NHCardShowcase projectTitle={"Project 3"} projectDate={"June 22, 2018"} projectText={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget neque euismod, tincidunt risus at, viverra lacus. In gravida nunc sed ex sollicitudin ultrices. Vivamus luctus, neque at facilisis euismod, turpis diam finibus libero, at malesuada risus eros a justo. Quisque dictum, quam vitae finibus blandit, erat urna imperdiet nisl."}/>
+          <NHCardShowcase projectTitle={"Project 4"} projectDate={"June 22, 2018"} projectText={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget neque euismod, tincidunt risus at, viverra lacus. In gravida nunc sed ex sollicitudin ultrices. Vivamus luctus, neque at facilisis euismod, turpis diam finibus libero, at malesuada risus eros a justo. Quisque dictum, quam vitae finibus blandit, erat urna imperdiet nisl."}/>
+          <NHCardShowcase projectTitle={"Project 5"} projectDate={"June 22, 2018"} projectText={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget neque euismod, tincidunt risus at, viverra lacus. In gravida nunc sed ex sollicitudin ultrices. Vivamus luctus, neque at facilisis euismod, turpis diam finibus libero, at malesuada risus eros a justo. Quisque dictum, quam vitae finibus blandit, erat urna imperdiet nisl."}/>
 
         </Content>
 
